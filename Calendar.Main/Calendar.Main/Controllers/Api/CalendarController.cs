1f51facc234dfc8737bc2b75459e8937d4a7589b
jackson <jackson@asus-rog-strix>
2024-07-26 22:47:55 +0800

    task: added endpoints

--- Calendar.Main/Calendar.Main/Controllers/Api/CalendarController.cs
+++ Calendar.Main/Calendar.Main/Controllers/Api/CalendarController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Calendar.Application.Requests.Commands;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Calendar.Main.Controllers.Api;
 
-[ApiController]
-[Route("api/[controller]")]
-public class CalendarController : ControllerBase
+public class CalendarController : ApiControllerBase
 {
+    [HttpPost("CreateEvent")]
+    public async Task CreateEventAsync(CreateEventCommand request) =>
+        await Mediator.Send(request);
     
+    [HttpPost("UpdateEvent")]
+    public async Task CreateEventAsync(UpdateEventCommand request) =>
+        await Mediator.Send(request);
 }
